d43facf2b0dc83c5863f80231b89ebe5c98a8e79
Adithi <68917173+AnuguAdithi@users.noreply.github.com>
2020-08-19 21:02:01 +0530

    Update views.py

--- v1/wordco/views.py
+++ v1/wordco/views.py
@@ -6,6 +6,6 @@ import operator
 def myhome(requests):
 	return render(requests,'wordco/myhome.html',{'name':'Adithi'})
 def aboutus(requests):
-	return render(requests,'wordco/aboutus.html',{'id':'userid@123'})
+	return render(requests,'wordco/aboutus.html',{'id':'1602-18-737-003'})
 def Myhobbies(requests):
 	return HttpResponse('<h1>My Hobbies Pages</h1>') 
